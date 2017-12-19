1acf50dddd0eadfd146b9b4f1f8961596fc7621d
Hank-Zhou <Hank-Zhou3510@outlook.com>
2017-12-19 12:46:10 +0800

    Py35.WebCreeper
    Py35.WebCreeper.BS4.1.5-豆瓣版

---
+++ Py35.WebCreeper.BS4.1.5-豆瓣版.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import time
+
+n = int(input("Enter the number of photo: "))
+html = urlopen(input("Enter the URL: "))
+
+bsObj = BeautifulSoup(html.read(), "html.parser")
+images = bsObj.findAll("img", {"src": re.compile("view")})
+
+for image in images:
+    print(image["src"].replace("m", "large").replace("ilargeg3.doubanio.colarge", "img3.doubanio.com").replace(
+        "ilargeg1.doubanio.colarge", "img1.doubanio.com"))
+    n += 1
+    imagefilename = "D:\Scrap Photo\\" + str(n) + ".jpg"
+    url = image["src"].replace("m", "large").replace("ilargeg3.doubanio.colarge", "img3.doubanio.com").replace(
+        "ilargeg1.doubanio.colarge", "img1.doubanio.com")
+    urllib.request.urlretrieve(url, imagefilename)
+    time.sleep(3)
+
+print("Ha!Ha! The Pictures Downloaded!")
+time.sleep(5)
+print("The number of pictures: ", n)
+print("Bye-Bye")
+time.sleep(3)
