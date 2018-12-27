d5768765e57d5d2ed462802a9912f1dc5c157d47
Hank-Zhou <Hank-Zhou3510@outlook.com>
2018-12-27 19:00:06 +0800

    Update Py35.WebCreeper.BS4.1.5.py
    change

--- Py35.WebCreeper.BS4.1.5.py
+++ Py35.WebCreeper.BS4.1.5.py
@@ -4,8 +4,8 @@ import urllib.request
 import re
 import time
 
-file = open('D:\\Scrap Photo\\number', 'r')         # 2018/4/21 19:19
-n = int(file.read())                                # 2018/4/21 19:19
+file = open('image/number', 'r')
+n = int(file.read())
 html = urlopen(input("Enter the URL: "))
 
 bsObj = BeautifulSoup(html.read(),"html.parser")
@@ -14,14 +14,14 @@ images = bsObj.findAll("img", {"src": re.compile(".jpg")})
 for image in images:
     print(image["src"].replace("/50/","/").replace("_hd","_r"))
     n += 1
-    imagefilename = "D:\\Scrap Photo\\" + str(n) + ".jpg"
+    imagefilename = "image/" + str(n) + ".jpg"
     url = image["src"].replace("/50/","/").replace("_hd","_r")
     urllib.request.urlretrieve(url,imagefilename)
     time.sleep(5)
 
 file.close()
 print("Ha!Ha! The Pictures Downloaded!")
-file2 = open('D:\\Scrap Photo\\number', 'w+')       # 2018/4/21 19:19
+file2 = open('image/number', 'w+')
 file2.write(str(n))
 file2.close()
 print("Bye-Bye")
